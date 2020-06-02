c25c96d34e84b243edf773442c191706bde2a73c
terencecaiga <terencecaiga@gmail.com>
2020-06-01 20:57:02 -0700

    Reindexed Ria's selection to making adding options later easier. Added consumables option.

--- 9010003.js
+++ 9010003.js
@@ -33,242 +33,245 @@ function action(mode, type, selection) {
 			status--;
 		if (status == 0) {
 	cm.sendSimple ("Pick a Category#e#d" +
-                 "#k\r\n#L80##rCommon" +
-                 "#k\r\n#L81##rWarrior" +
-                 "#k\r\n#L82##rMagician" +
-                 "#k\r\n#L83##rArcher" +
-                 "#k\r\n#L84##rThief" +
-                 "#k\r\n#L85##rPirate" +
-                 "#k\r\n#L86##rETC Items");
-	  } else if (selection == 80) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"#k\r\n#L0##rEarrings" +
-			"#k\r\n#L1##rNecklaces and Face Accessories" +
-			"#k\r\n#L2##rCapes" +
-			"#k\r\n#L3##rOveralls" +
-			"#k\r\n#L4##rGloves" +
-			"#k\r\n#L5##rShoes" +
-			"#k\r\n#L6##rMaple Weapons");
-	  } else if (selection == 81) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"#k\r\n#L7##bHats" +
-			"#k\r\n#L8##bTops" +
-			"#k\r\n#L9##bBottoms" +
-			"#k\r\n#L10##bOveralls" +
-			"#l\r\n#L11##bGloves" +
-			"#l\r\n#L12##bShields" +
-			"#l\r\n#L13##bShoes" +
-			"#l\r\n#L14##b1H Axes" +
-			"#k\r\n#L15##b2H Axes" +
-			"#k\r\n#L16##b1H BWs" +
-			"#k\r\n#L17##b2H BWs" +
-			"#k\r\n#L18##b1H Swords" +
-			"#k\r\n#L19##b2H Swords" +
-			"#k\r\n#L20##b2H Spears" +
-			"#k\r\n#L21##bPole Arms");
-	  } else if (selection == 82) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"#k\r\n#L22##dHats" +
-			"#k\r\n#L23##dOveralls" +
-			"#k\r\n#L24##dGloves" +
-			"#k\r\n#L25##dShields" +
-			"#k\r\n#L26##dShoes" +
-			"#k\r\n#L27##dWands" +
-			"#k\r\n#L28##dStaffs");
-	  } else if (selection == 83) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"#k\r\n#L29##gHats" +
-			"#k\r\n#L30##gOveralls" +
-			"#k\r\n#L31##gGloves" +
-			"#k\r\n#L32##gShoes" +
-			"#k\r\n#L33##gBows" +
-			"#k\r\n#L34##gCrossbows" +
-			"#k\r\n#L35##gArrows");
-	  } else if (selection == 84) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"#k\r\n#L36##bHats" +
-			"#k\r\n#L37##bTops" +
-			"#k\r\n#L38##bBottoms" +
-			"#k\r\n#L39##bOveralls" +
-			"#k\r\n#L40##bGloves" +
-			"#k\r\n#L41##bShields" +
-			"#k\r\n#L42##bShoes" +
-			"#k\r\n#L43##bDaggers" +
-			"#k\r\n#L44##bClaws" +
-			"#k\r\n#L45##bThrowing Stars");
-	  } else if (selection == 85) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"#k\r\n#L46#Hats" +
-			"#k\r\n#L47#Overalls" +
-			"#k\r\n#L48#Gloves" +
-			"#k\r\n#L49#Shoes" +
-			"#k\r\n#L50#Weapons" +
-			"#k\r\n#L51#Bullets and Capsules");
-	  } else if (selection == 86) {
-               cm.sendSimple ("Pick a Category #e#d" +
-			"\r\n#L52#Consumables" +
-			"\r\n#L53#Scrolls");
+                 "#k\r\n#L0##rCommon" +
+                 "#k\r\n#L1##rWarrior" +
+                 "#k\r\n#L2##rMagician" +
+                 "#k\r\n#L3##rArcher" +
+                 "#k\r\n#L4##rThief" +
+                 "#k\r\n#L5##rPirate" +
+                 "#k\r\n#L6##rETC Items");
 	  } else if (selection == 0) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"#k\r\n#L100##rEarrings" +
+			"#k\r\n#L101##rNecklaces and Face Accessories" +
+			"#k\r\n#L102##rCapes" +
+			"#k\r\n#L103##rOveralls" +
+			"#k\r\n#L104##rGloves" +
+			"#k\r\n#L105##rShields" +
+			"#k\r\n#L106##rShoes" +
+			"#k\r\n#L107##rMaple Weapons");
+	  } else if (selection == 1) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"#k\r\n#L200##bHats" +
+			"#k\r\n#L201##bTops" +
+			"#k\r\n#L202##bBottoms" +
+			"#k\r\n#L203##bOveralls" +
+			"#l\r\n#L204##bGloves" +
+			"#l\r\n#L205##bShields" +
+			"#l\r\n#L206##bShoes" +
+			"#l\r\n#L207##b1H Axes" +
+			"#k\r\n#L208##b2H Axes" +
+			"#k\r\n#L209##b1H BWs" +
+			"#k\r\n#L210##b2H BWs" +
+			"#k\r\n#L211##b1H Swords" +
+			"#k\r\n#L212##b2H Swords" +
+			"#k\r\n#L213##b2H Spears" +
+			"#k\r\n#L214##bPole Arms");
+	  } else if (selection == 2) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"#k\r\n#L300##dHats" +
+			"#k\r\n#L301##dOveralls" +
+			"#k\r\n#L302##dGloves" +
+			"#k\r\n#L303##dShields" +
+			"#k\r\n#L304##dShoes" +
+			"#k\r\n#L305##dWands" +
+			"#k\r\n#L306##dStaffs");
+	  } else if (selection == 3) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"#k\r\n#L400##gHats" +
+			"#k\r\n#L401##gOveralls" +
+			"#k\r\n#L402##gGloves" +
+			"#k\r\n#L403##gShoes" +
+			"#k\r\n#L404##gBows" +
+			"#k\r\n#L405##gCrossbows" +
+			"#k\r\n#L406##gArrows");
+	  } else if (selection == 4) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"#k\r\n#L500##bHats" +
+			"#k\r\n#L501##bTops" +
+			"#k\r\n#L502##bBottoms" +
+			"#k\r\n#L503##bOveralls" +
+			"#k\r\n#L504##bGloves" +
+			"#k\r\n#L505##bShields" +
+			"#k\r\n#L506##bShoes" +
+			"#k\r\n#L507##bDaggers" +
+			"#k\r\n#L508##bClaws" +
+			"#k\r\n#L509##bThrowing Stars");
+	  } else if (selection == 5) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"#k\r\n#L600#Hats" +
+			"#k\r\n#L601#Overalls" +
+			"#k\r\n#L602#Gloves" +
+			"#k\r\n#L603#Shoes" +
+			"#k\r\n#L604#Weapons" +
+			"#k\r\n#L605#Bullets and Capsules");
+	  } else if (selection == 6) {
+               cm.sendSimple ("Pick a Category #e#d" +
+			"\r\n#L700#Consumables" +
+			"\r\n#L701#Scrolls");
+	  } else if (selection == 100) {
 		cm.openShopNPC (6000);
 		cm.dispose();
-	  } else if (selection == 1) {
+	  } else if (selection == 101) {
 		cm.openShopNPC (6001);
 		cm.dispose();
-	  } else if (selection == 2) {
+	  } else if (selection == 102) {
 		cm.openShopNPC (6002);
 		cm.dispose();
-	  } else if (selection == 3) {
+	  } else if (selection == 103) {
 		cm.openShopNPC (6003);
 		cm.dispose();
-	  } else if (selection == 4) {
+	  } else if (selection == 104) {
 		cm.openShopNPC (6004);
 		cm.dispose();
-	  } else if (selection == 5) {
+	  } else if (selection == 105) {
 		cm.openShopNPC (6005);
 		cm.dispose();
-	  } else if (selection == 6) {
+	  } else if (selection == 106) {
 		cm.openShopNPC (6006);
 		cm.dispose();
-	  } else if (selection == 7) {
+	  } else if (selection == 107) {
+		cm.openShopNPC (6007);
+		cm.dispose();
+	  } else if (selection == 200) {
 		cm.openShopNPC (6100);
 		cm.dispose();
-	  } else if (selection == 8) {
+	  } else if (selection == 201) {
 		cm.openShopNPC (6101);
 		cm.dispose();
-	  } else if (selection == 9) {
+	  } else if (selection == 202) {
 		cm.openShopNPC (6102);
 		cm.dispose();
-	  } else if (selection == 10) {
+	  } else if (selection == 203) {
 		cm.openShopNPC (6103);
 		cm.dispose();
-	  } else if (selection == 11) {
+	  } else if (selection == 204) {
 		cm.openShopNPC (6104);
 		cm.dispose();
-	  } else if (selection == 12) {
+	  } else if (selection == 205) {
 		cm.openShopNPC (6105);
 		cm.dispose();
-	  } else if (selection == 13) {
+	  } else if (selection == 206) {
 		cm.openShopNPC (6106);
 		cm.dispose();
-	  } else if (selection == 14) {
+	  } else if (selection == 207) {
 		cm.openShopNPC (6107);
 		cm.dispose();
-	  } else if (selection == 15) {
+	  } else if (selection == 208) {
 		cm.openShopNPC (6108);
 		cm.dispose();
-	  } else if (selection == 16) {
+	  } else if (selection == 209) {
 		cm.openShopNPC (6109);
 		cm.dispose();
-	  } else if (selection == 17) {
+	  } else if (selection == 210) {
 		cm.openShopNPC (6110);
 		cm.dispose();
-	  } else if (selection == 18) {
+	  } else if (selection == 211) {
 		cm.openShopNPC (6111);
 		cm.dispose();
-	  } else if (selection == 19) {
+	  } else if (selection == 212) {
 		cm.openShopNPC (6112);
 		cm.dispose();
-	  } else if (selection == 20) {
+	  } else if (selection == 213) {
 		cm.openShopNPC (6113);
 		cm.dispose();
-	  } else if (selection == 21) {
+	  } else if (selection == 214) {
 		cm.openShopNPC (6114);
 		cm.dispose();
-	  } else if (selection == 22) {
+	  } else if (selection == 300) {
 		cm.openShopNPC (6200);
 		cm.dispose();
-	  } else if (selection == 23) {
+	  } else if (selection == 301) {
 		cm.openShopNPC (6201);
 		cm.dispose();
-	  } else if (selection == 24) {
+	  } else if (selection == 302) {
 		cm.openShopNPC (6202);
 		cm.dispose();
-	  } else if (selection == 25) {
+	  } else if (selection == 303) {
 		cm.openShopNPC (6203);
 		cm.dispose();
-	  } else if (selection == 26) {
+	  } else if (selection == 304) {
 		cm.openShopNPC (6204);
 		cm.dispose();
-	  } else if (selection == 27) {
+	  } else if (selection == 305) {
 		cm.openShopNPC (6205);
 		cm.dispose();
-	  } else if (selection == 28) {
+	  } else if (selection == 306) {
 		cm.openShopNPC (6206);
 		cm.dispose();
-	  } else if (selection == 29) {
+	  } else if (selection == 400) {
 		cm.openShopNPC (6300);
 		cm.dispose();
-	  } else if (selection == 30) {
+	  } else if (selection == 401) {
 		cm.openShopNPC (6301);
 		cm.dispose();
-	  } else if (selection == 31) {
+	  } else if (selection == 402) {
 		cm.openShopNPC (6302);
 		cm.dispose();
-	  } else if (selection == 32) {
+	  } else if (selection == 403) {
 		cm.openShopNPC (6303);
 		cm.dispose();
-	  } else if (selection == 33) {
+	  } else if (selection == 404) {
 		cm.openShopNPC (6304);
 		cm.dispose();
-	  } else if (selection == 34) {
+	  } else if (selection == 405) {
 		cm.openShopNPC (6305);
 		cm.dispose();
-	  } else if (selection == 35) {
+	  } else if (selection == 406) {
 		cm.openShopNPC (6306);
 		cm.dispose();
-	  } else if (selection == 36) {
+	  } else if (selection == 500) {
 		cm.openShopNPC (6400);
 		cm.dispose();
-	  } else if (selection == 37) {
+	  } else if (selection == 501) {
 		cm.openShopNPC (6401);
 		cm.dispose();
-	  } else if (selection == 38) {
+	  } else if (selection == 502) {
 		cm.openShopNPC (6402);
 		cm.dispose();
-	  } else if (selection == 39) {
+	  } else if (selection == 503) {
 		cm.openShopNPC (6403);
 		cm.dispose();
-	  } else if (selection == 40) {
+	  } else if (selection == 504) {
 		cm.openShopNPC (6404);
 		cm.dispose();
-	  } else if (selection == 41) {
+	  } else if (selection == 505) {
 		cm.openShopNPC (6405);
 		cm.dispose();
-	  } else if (selection == 42) {
+	  } else if (selection == 506) {
 		cm.openShopNPC (6406);
 		cm.dispose();
-	  } else if (selection == 43) {
+	  } else if (selection == 507) {
 		cm.openShopNPC (6407);
 		cm.dispose();
-	  } else if (selection == 44) {
+	  } else if (selection == 508) {
 		cm.openShopNPC (6408);
 		cm.dispose();
-	  } else if (selection == 45) {
+	  } else if (selection == 509) {
 		cm.openShopNPC (6409);
 		cm.dispose();
-	  } else if (selection == 46) {
+	  } else if (selection == 600) {
 		cm.openShopNPC (6500);
 		cm.dispose();
-	  } else if (selection == 47) {
+	  } else if (selection == 601) {
 		cm.openShopNPC (6501);
 		cm.dispose();
-	  } else if (selection == 48) {
+	  } else if (selection == 602) {
 		cm.openShopNPC (6502);
 		cm.dispose();
-	  } else if (selection == 49) {
+	  } else if (selection == 603) {
 		cm.openShopNPC (6503);
 		cm.dispose();
-	  } else if (selection == 50) {
+	  } else if (selection == 604) {
 		cm.openShopNPC (6504);
 		cm.dispose();
-	  } else if (selection == 51) {
+	  } else if (selection == 605) {
 		cm.openShopNPC (6505);
 		cm.dispose();
-	  } else if (selection == 52) {
-		// cm.openShopNPC (6600);
-		cm.sendOk("Under construction");
+	  } else if (selection == 700) {
+		cm.openShopNPC (6600);
 		cm.dispose();
-	  } else if (selection == 53) {
+	  } else if (selection == 701) {
 		cm.openShopNPC (6601);
 		cm.dispose();
 	  }
